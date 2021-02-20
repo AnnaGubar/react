e660e6babf47cfd5d5c95d342918b43e01d13077
Anna Gubar <68061285+AnnaGubar@users.noreply.github.com>
2021-02-20 21:07:42 +0200

    added

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./Footer.module.css";
+
+const Footer = () => (
+  <footer className={styles.footer}>
+    <p>Footer</p>
+    <p>Value</p>
+  </footer>
+);
+
+export default Footer;
